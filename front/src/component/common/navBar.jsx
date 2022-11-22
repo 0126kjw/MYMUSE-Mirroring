4fa2fb008c8aa42249f593228a294938456efe4a
jpra2021 <jthvmcm2021@cuk.edu>
2022-11-22 11:34:19 +0900

    [FE/REMOVE] jsons change app,index jsx

--- front/src/component/common/navBar.jsx
+++ front/src/component/common/navBar.jsx
@@ -2,37 +2,50 @@ import Link from 'next/link'
 import styled from '@emotion/styled'
 
 const NavBarLayout = styled.div`
-    ul{
-        margin:0px;
-        
-        padding-left:0px;
-        display:flex;
-        justify-content: space-around;
-        background-color:#404048;
-        height:50px;
-        line-height:50px;
-        
-        span{
-            color:#FFFFFF;
-        }
-        li{
-            list-style:none;
-            &:hover{
-                cursor: pointer
-            }
-        }
-    }
+	ul {
+		margin: 0px;
 
+		padding-left: 0px;
+		display: flex;
+		justify-content: space-around;
+		background-color: #404048;
+		height: 50px;
+		line-height: 50px;
+
+		span {
+			color: #ffffff;
+		}
+		li {
+			list-style: none;
+			&:hover {
+				cursor: pointer;
+			}
+		}
+	}
 `
 
 export default function NavBar() {
 	return (
 		<NavBarLayout>
 			<ul>
-				<li><Link href='/'><span>Home</span></Link></li>
-				<li><Link href='/sub/search'> <span>Search</span> </Link></li>
-				<li><Link href='/sub/zido'> <span>SeoulMap</span> </Link></li>
-				<li><Link href='/sub/rank'> <span>Ranking</span> </Link></li>
+				<li>
+					<Link href='/'>
+						<span>Home</span>
+					</Link>
+				</li>
+				<li>
+					<Link href='/sub/search'>
+						{' '}
+						<span>Search</span>{' '}
+					</Link>
+				</li>
+				{/* <li><Link href='/sub/zido'> <span>SeoulMap</span> </Link></li> */}
+				<li>
+					<Link href='/sub/rank'>
+						{' '}
+						<span>Ranking</span>{' '}
+					</Link>
+				</li>
 			</ul>
 		</NavBarLayout>
 	)
