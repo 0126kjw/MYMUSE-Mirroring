6415082b43426f161f6bd03dbbf944f92d026a2c
[AI5/2반] 김진우 <happy150523@gmail.com>
2022-11-25 01:24:01 +0900

    [BE/FEAT] Add map controller & service

--- back/src/map/map.controller.ts
+++ back/src/map/map.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpCode, Param, Res } from '@nestjs/common';
+import { MapService } from './map.service';
 
 @Controller('map')
-export class MapController {}
+export class MapController {
+  constructor(private mapService: MapService) {}
+
+  @Get('/:guid')
+  async getMapInfo(@Param('guid') id: string, @Res() res): Promise<object> {
+    const mapData = this.mapService.findMap(id);
+    return await res.status(200).json(mapData);
+  }
+
+  @Get('/:guid/pins')
+  async getPin(@Param('guid') id: string, @Res() res): Promise<object> {
+    const pinData = this.mapService.findPlace(id);
+    return await res.status(200).json(pinData);
+  }
+}
