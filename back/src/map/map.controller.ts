4560a80413961566f0f2bac1dca64642e967dd1d
[AI5/2반] 김진우 <happy150523@gmail.com>
2022-11-25 23:40:35 +0900

    [BE/REFACT] change api decorator

--- back/src/map/map.controller.ts
+++ back/src/map/map.controller.ts
@@ -1,5 +1,10 @@
 import { Controller, Get, Param, Res } from '@nestjs/common';
-import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { getMapInfo } from './dto/getMapInfo.dto';
 import { getPin } from './dto/getPin.dto';
 import { MapService } from './map.service';
@@ -10,11 +15,10 @@ export class MapController {
   constructor(private mapService: MapService) {}
 
   @ApiOperation({ summary: '자치구 데이터 가져오기' })
-  @ApiResponse({
-    status: 200,
+  @ApiOkResponse({
     description: 'guid 값과 동일한 자치구의 데이터를 반환한다.',
   })
-  @ApiResponse({ status: 404, description: 'NotFound' })
+  @ApiNotFoundResponse({ description: 'NotFound' })
   @Get('/:guid')
   async getMapInfo(
     @Param() getMapInfoDto: getMapInfo,
@@ -27,11 +31,10 @@ export class MapController {
   }
 
   @ApiOperation({ summary: '자치구 별 모든 장소 조회' })
-  @ApiResponse({
-    status: 200,
+  @ApiOkResponse({
     description: 'guid 값과 동일한 자치구의 장소 데이터를 반환한다.',
   })
-  @ApiResponse({ status: 404, description: 'NotFound' })
+  @ApiNotFoundResponse({ description: 'NotFound' })
   @Get('/:guid/pins')
   async getPin(@Param() getPinDto: getPin, @Res() res): Promise<void> {
     const pinData = this.mapService.findPlace(getPinDto.guid);
