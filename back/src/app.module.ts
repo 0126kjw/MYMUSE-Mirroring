ea44c5818e93f5005df6f396cc88f20c6e2bcec2
yb <cd.yb0229@gmail.com>
2022-11-25 21:47:42 +0900

    REMOVE: File, app.module.ts contents delete
    app.controller.spec.ts
    app.controller.ts
    app.service.ts

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,8 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [
@@ -11,7 +9,7 @@ import { AppService } from './app.service';
     }),
     MongooseModule.forRoot(process.env.MONGODB_URL),
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
