a58bf39f968b7d94bd6dceb08e1877b3049900ba
jpra2021 <jthvmcm2021@cuk.edu>
2022-11-26 05:04:56 +0900

    [FE/add] ErrorBoundary

--- front/src/pages/_app.jsx
+++ front/src/pages/_app.jsx
@@ -1,14 +1,18 @@
-import '../styles/globals.css';
+//style
 import AppLayout from 'component/AppLayout';
 import { GlobalStyles } from 'styles/globalStyle';
+//for error
+import { ErrorBoundary } from 'react-error-boundary';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			{GlobalStyles}
-			<AppLayout>
-				<Component {...pageProps} />
-			</AppLayout>
+			<ErrorBoundary faillback={<div>Loading...</div>}>
+				{GlobalStyles}
+				<AppLayout>
+					<Component {...pageProps} />
+				</AppLayout>
+			</ErrorBoundary>
 		</>
 	);
 }
