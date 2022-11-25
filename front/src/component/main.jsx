404b876fa3875d7a67f5d061f6fe308d7e40aeaa
jpra2021 <jthvmcm2021@cuk.edu>
2022-11-25 11:55:42 +0900

    [fe] test

--- front/src/component/main.jsx
+++ front/src/component/main.jsx
@@ -4,10 +4,16 @@ import {
 	Logosection,
 } from 'styles/pageStyles/mainStyle';
 import Image from 'next/image';
+
+import { css } from '@emotion/react';
 // component
 
 import SearchBar from './SearchBar';
 import Slider from './Slider';
+const Section = css`
+	min-width: 1200px;
+	height: 900px;
+`;
 
 const Main = () => {
 	return (
@@ -15,16 +21,33 @@ const Main = () => {
 			<MainContainer>
 				<MainTitleContainer>
 					<Logosection>
-						<Image
+						{/* <Image
 							src='imgaes/siteLogo.png'
 							alt='logo_title'
 							layout='fill'
 							objectFit='contain'
 							unoptimized={true}
-						/>
+						/> */}
 					</Logosection>
 				</MainTitleContainer>
-				<Slider></Slider>
+				<section
+					css={css`
+						${Section}
+					`}
+				>
+					<Slider />
+				</section>
+				<section
+					css={css`
+						${Section}
+					`}
+				></section>
+				<section
+					css={css`
+						${Section}
+					`}
+				></section>
+
 				{/* <AiBot></AiBot> */}
 			</MainContainer>
 		</>
