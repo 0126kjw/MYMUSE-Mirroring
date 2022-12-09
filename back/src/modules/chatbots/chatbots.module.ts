0a03268ed6150cb493d3864755d318598b1b9b04
yb <cd.yb0229@gmail.com>
2022-12-09 10:25:39 +0900

    FIX: Chatbot editing

--- back/src/modules/chatbots/chatbots.module.ts
+++ back/src/modules/chatbots/chatbots.module.ts
@@ -9,6 +9,7 @@ import { MuseumModule } from '../museums/museums.module';
 import { Museum, MuseumSchema } from '../museums/schemas/museum.schema';
 import { ChatbotController } from './chatbots.controller';
 import { ChatbotService } from './chatbots.service';
+import { Chatbot, ChatbotSchema } from './schemas/chatbot.schema';
 
 @Module({
   imports: [
@@ -17,6 +18,7 @@ import { ChatbotService } from './chatbots.service';
     MongooseModule.forFeature([
       { name: Museum.name, schema: MuseumSchema },
       { name: Exhibition.name, schema: ExhibitionSchema },
+      { name: Chatbot.name, schema: ChatbotSchema },
     ]),
   ],
   controllers: [ChatbotController],
