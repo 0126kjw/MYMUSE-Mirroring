be3718b124cdba7bea6396fd7e6a6496f8e1307d
윤시준 <webcodur@gmail.com>
2022-12-11 16:33:25 +0900

    [FEAT] menu is created by map from now on

--- front/src/component/common/NavBar.jsx
+++ front/src/component/common/NavBar.jsx
@@ -5,34 +5,27 @@ import Link from 'next/link';
 
 const NavBar = () => {
 	const loc = useRecoilValue(currentLoc);
+	const menu = [
+		{ korName: '홈', route: '/' },
+		{ korName: '지도', route: '/map' },
+		{ korName: '검색', route: '/search' },
+		{ korName: '진행중인 전시회', route: '/popular' },
+	];
 
 	return (
 		<NaviContainer>
 			<NavBarLayout>
 				<ul>
-					<div className={loc === '/' ? 'selectedColor' : 'defaultColor'}>
-						<Link href='/' style={{ textDecoration: 'none' }}>
-							<div className='white'>홈</div>
-						</Link>
-					</div>
-
-					<div className={loc === '/map' ? 'selectedColor' : 'defaultColor'}>
-						<Link href='/map' style={{ textDecoration: 'none' }}>
-							<div className='white'>지도</div>
-						</Link>
-					</div>
-
-					<div className={loc === '/search' ? 'selectedColor' : 'defaultColor'}>
-						<Link href='/search' style={{ textDecoration: 'none' }}>
-							<div className='white'>검색</div>
-						</Link>
-					</div>
-
-					<div className={loc === '/popular' ? 'selectedColor' : 'defaultColor'}>
-						<Link href='/popular' style={{ textDecoration: 'none' }}>
-							<div className='white'>진행중인 전시회</div>
-						</Link>
-					</div>
+					{menu.map((item, idx) => (
+						<div
+							key={`pcMenu${idx}`}
+							className={loc === item.route ? 'selectedColor' : 'defaultColor'}
+						>
+							<Link href={item.route} style={{ textDecoration: 'none' }}>
+								<div className='white'>{item.korName}</div>
+							</Link>
+						</div>
+					))}
 				</ul>
 			</NavBarLayout>
 		</NaviContainer>
