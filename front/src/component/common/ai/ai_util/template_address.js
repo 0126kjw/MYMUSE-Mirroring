9c2b960a908a9a284485ccad1650a5a7cb38e4b5
윤시준 <webcodur@gmail.com>
2022-12-14 20:43:54 +0900

    [style] aibot template color

--- front/src/component/common/ai/ai_util/template_address.js
+++ front/src/component/common/ai/ai_util/template_address.js
@@ -1,12 +1,12 @@
 const facilityAddressTemp = (name, newAddress, oldAddress) => {
 	return `
-        <table border="1" bordercolor="blue" width="600" height="200" align="center">
-        <tr bgcolor="blue" align="center">
+    <table border="1" bordercolor="brown" width="600" height="200" align="center">
+        <tr bgcolor="#D9BC8C" align="center">
             <p>
-                <td colspan="3" span style="color:white">${name} 주소</td>
+                <td colspan="3" span style="color:black; font-weight:bold">${name} 주소</td>
             </p>
         </tr>
-        <tr align="center" bgcolor="skybule">
+        <tr align="center" bgcolor="beige">
             <td>분류</td>
             <td>내용</td>
         </tr>
